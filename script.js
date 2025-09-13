610e2ad093a422df4f09edf7050ff9a5c91555ea
vivekkalal09 <vivekkalal09@gmail.com>
2025-09-13 12:27:13 +0530

    final quiz-app

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const questions = [
     {
-        questions: "Midinnings does?",
+        question: "Midinnings does?",
         answers: [
             { text: "News Marketing Company", correct: false },
             { text: "Digital Marketing Company", correct: true },
@@ -10,7 +10,7 @@ const questions = [
         ]
     },
     {
-        questions: "Who Manages N8N workflows?",
+        question: "Who Manages N8N workflows?",
         answers: [
             { text: "Vivek Kalal", correct: true },
             { text: "Rahul goswami", correct: false },
@@ -19,7 +19,7 @@ const questions = [
         ]
     },
     {
-        questions: "How many client it handles?",
+        question: "How many client it handles?",
         answers: [
             { text: "20", correct: false },
             { text: "46", correct: false },
@@ -36,8 +36,75 @@ let currentQuestionIndex = 0;
 let score = 0;
 
 function startQuiz() {
+    resetState();
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
 }
+
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    })
+}
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+
+}
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    resetState();
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+
+};
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    }
+    else {
+        startQuiz();
+    }
+})
+startQuiz();
